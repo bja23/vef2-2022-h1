4a03f1011e2cd43a4d335b1732003277e2d03507
bja23 <42986015+bja23@users.noreply.github.com>
2022-03-05 17:39:49 +0000

    gagnagrunnur og test gögn

---
+++ sql/insert.sql
@@ -0,0 +1,52 @@
+
+INSERT INTO users (name, username, password, isAdmin) VALUES ('Björgvin','admin', '$2a$11$pgj3.zySyFOvIQEpD7W6Aund1Tw.BFarXxgLJxLbrzIv/4Nteisii', TRUE);
+INSERT INTO users (name, username, password) VALUES ('NOTBjörgvin','notadmin', '$2a$11$pgj3.zySyFOvIQEpD7W6Aund1Tw.BFarXxgLJxLbrzIv/4Nteisii');
+
+INSERT INTO flokkur (title)
+VALUES("Pizza");
+INSERT INTO flokkur (title)
+VALUES("Hamborgari");
+INSERT INTO flokkur (title)
+VALUES("Pylsa");
+INSERT INTO flokkur (title)
+VALUES("Drykkur");
+INSERT INTO flokkur (title)
+VALUES("Nammi");
+
+INSERT INTO vorur (title, price, description, image, "flokkur")
+VALUES ("margarita", 2000, "ostur og sosa", "https://res.cloudinary.com/dqhgwsiuq/image/upload/v1646500948/margarita_silv8z.jpg", 1);
+INSERT INTO vorur (title, price, description, image, "flokkur")
+VALUES ("pepperoni", 2100, "ostur, sosa og pepperonu", "https://res.cloudinary.com/dqhgwsiuq/image/upload/v1646500948/pepperoni_t0mako.jpg", 1);
+INSERT INTO vorur (title, price, description, image, "flokkur")
+VALUES ("hawaii", 2100, "ostur, sosa, annanas og skínka", "https://res.cloudinary.com/dqhgwsiuq/image/upload/v1646500948/hawaiian-pizza-recipe-605894-2_clcqmt.jpg", 1);
+
+INSERT INTO vorur (title, price, description, image, "flokkur")
+VALUES ("ostlausborgari", 2000, "ostlaus hamborgari", "https://res.cloudinary.com/dqhgwsiuq/image/upload/v1646500948/ostlaugborgari_ah3wvs.jpg", 2);
+INSERT INTO vorur (title, price, description, image, "flokkur")
+VALUES ("ostborgari", 2200, "ost hamborgari", "https://res.cloudinary.com/dqhgwsiuq/image/upload/v1646500948/ostborgari_yh6ddk.jpg", 2);
+INSERT INTO vorur (title, price, description, image, "flokkur")
+VALUES ("baconborgari", 2500, "hamborgari með ost og bacon", "https://res.cloudinary.com/dqhgwsiuq/image/upload/v1646500948/baconborgari_k5fm5s.jpg", 2);
+
+INSERT INTO vorur (title, price, description, image, "flokkur")
+VALUES ("tompylsa", 1000, "pylsa með engu ", "https://res.cloudinary.com/dqhgwsiuq/image/upload/v1646500948/puslamedengu_dk8syg.jpg", 3);
+INSERT INTO vorur (title, price, description, image, "flokkur")
+VALUES ("tomatpylsa", 1200, "pysla með tómatsósu", "https://res.cloudinary.com/dqhgwsiuq/image/upload/v1646500948/pylsamedTomatsosu_nsvisj.jpg", 3);
+INSERT INTO vorur (title, price, description, image, "flokkur")
+VALUES ("sineppylsa", 1500, "pylsa með sinepi", "https://res.cloudinary.com/dqhgwsiuq/image/upload/v1646500948/sinepspylsa_riyqrn.png", 3);
+
+INSERT INTO vorur (title, price, description, image, "flokkur")
+VALUES ("bounty", 400, "bounty súkkulaði", "https://res.cloudinary.com/dqhgwsiuq/image/upload/v1646500947/bounty_gknzdr.png", 4);
+INSERT INTO vorur (title, price, description, image, "flokkur")
+VALUES ("mars", 500, "mars súkkulaði", "https://res.cloudinary.com/dqhgwsiuq/image/upload/v1646500947/mars_nfc33t.jpg", 4);
+INSERT INTO vorur (title, price, description, image, "flokkur")
+VALUES ("twix", 450, "twix súkkulaði", "https://res.cloudinary.com/dqhgwsiuq/image/upload/v1646500947/twix_omnpyr.jpg", 4);
+
+INSERT INTO vorur (title, price, description, image, "flokkur")
+VALUES ("vatn", 200, "vatn 500ml", "https://res.cloudinary.com/dqhgwsiuq/image/upload/v1646500947/vatn_zdgrom.jpg", 5);
+INSERT INTO vorur (title, price, description, image, "flokkur")
+VALUES ("fanta", 400, "fanta 500ml", "https://res.cloudinary.com/dqhgwsiuq/image/upload/v1646500947/fanta_olgxrh.jpg", 5);
+INSERT INTO vorur (title, price, description, image, "flokkur")
+VALUES ("pepsi", 400, "pepsi 500ml", "https://res.cloudinary.com/dqhgwsiuq/image/upload/v1646500947/pepsi_iq07g9.jpg", 5);
+
+
+
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,10 +1,25 @@
+DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS flokkur;
 DROP TABLE IF EXISTS vorur;
+DROP TABLE IF EXISTS karfa;
+DROP TABLE IF EXISTS linaIKorfu;
+DROP TABLE IF EXISTS pontun;
+DROP TABLE IF EXISTS linaIPontun;
+DROP TABLE IF EXISTS stodurPantana;
+DROP TABLE IF EXISTS pontunState;
+
+CREATE TABLE users (
+  id serial primary key,
+  name character varying(64) NOT NULL,
+  username character varying(64) NOT NULL UNIQUE,
+  password character varying(255) NOT NULL,
+  isAdmin BOOLEAN DEFAULT FALSE
+);
 
 CREATE TABLE flokkur (
   id serial primary key,
   title character VARCHAR (64) NOT NULL UNIQUE
-)
+);
 
 CREATE TABLE vorur (
   id serial primary key,
@@ -16,14 +31,45 @@ CREATE TABLE vorur (
   CONSTRAINT "flokkur" FOREIGN KEY ("flokkur") REFERENCES flokkur (id) ON DELETE CASCADE,
   created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
   updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp
-
-)
+);
 
 CREATE TABLE karfa (
-  id 
-)
+  id character VARCHAR(255) NOT null UNIQUE,
+  created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp
+);
 
+CREATE TABLE linaIKorfu (
+  "vara" INTEGER NOT NULL,
+  CONSTRAINT "vara" FOREIGN KEY ("vara") REFERENCES vorur (id) ON DELETE CASCADE,
+  "vara" INTEGER NOT NULL,
+  CONSTRAINT "karfa" FOREIGN KEY ("karfa") REFERENCES karfa (id) ON DELETE CASCADE,
+  fjoldi INTEGER NOT NULL,
+  CHECK (fjoldi > 0)
+);
 
+CREATE TABLE pontun (
+  id character VARCHAR(255) NOT NULL UNIQUE,
+  created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
+  name VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE linaIPontun (
+  "vara" INTEGER NOT NULL,
+  CONSTRAINT "vara" FOREIGN KEY ("vara") REFERENCES vorur (id) ON DELETE CASCADE,
+  "vara" INTEGER NOT NULL,
+  CONSTRAINT "karfa" FOREIGN KEY ("karfa") REFERENCES karfa (id) ON DELETE CASCADE,
+  fjoldi INTEGER NOT NULL,
+  CHECK (fjoldi > 0)
+);
+
+CREATE TYPE pontunState AS ENUM ('NEW', 'PREPARE', 'COOKING', 'READY', ' FINISHED');
+
+CREATE TABLE stodurPantana (
+  "pontun" INTEGER NOT NULL,
+  CONSTRAINT "pontun" FOREIGN KEY ("pontun") REFERENCES pontun (id) ON DELETE CASCADE,
+  state stodurPantana NOT NULL,
+  created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp
+);
 
 DROP ROLE IF EXISTS "vef2-user";
 CREATE USER "vef2-user" WITH ENCRYPTED PASSWORD '123';
